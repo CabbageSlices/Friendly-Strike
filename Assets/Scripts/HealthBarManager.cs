75841f586d5577723e0b4ab8d362b6f5c4e411aa
Irelevance <nimesh.shrestha@hotmail.com>
2016-12-27 14:50:10 -0800

    Added healthbar

---
+++ Assets/Scripts/HealthBarManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//handles the graphics of the health bar
+public class HealthBarManager : MonoBehaviour {
+
+    //rect transform of the part of the healthbar that represents the amount of health that is remaining (green part)
+    public RectTransform healthValueIndicator;
+
+	// Use this for initialization
+	void Start () {
+
+        if (healthValueIndicator == null)
+            Debug.Log("HealthBarManager missing healthValueIndicator reference");
+	}
+	
+    //sets the size of the green part of the health bar to the given fraction of total health
+    //health can't be below 0 or above 1
+	public void setHealthValue(float fractionHealthRemaining) {
+
+        fractionHealthRemaining = Mathf.Clamp(fractionHealthRemaining, 0, 1);
+
+        healthValueIndicator.localScale = new Vector3(fractionHealthRemaining, 1, 1);
+    }
+
+    public void hideHealthBar() {
+
+        gameObject.SetActive(false);
+    }
+
+    public void showHealthBar() {
+
+        gameObject.SetActive(true);
+    }
+}
--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -5,11 +5,15 @@ using System.Collections;
 public class HealthManager : MonoBehaviour {
 
     public int initialHealth = 100;
+    public HealthBarManager healthBarManager;//script that handles the healthbar UI changes
 
     private int currentHealth;
 
 	// Use this for initialization
 	void Start () {
+
+        if (healthBarManager == null)
+            Debug.LogWarning("HealthManager is missing healthBarManager reference");
 
         currentHealth = initialHealth;
 	}
@@ -20,10 +24,25 @@ public class HealthManager : MonoBehaviour {
 
         if (currentHealth < 0)
             currentHealth = 0;
+
+        updateHealthBar();        
     }
 
     public void restoreHealth() {
 
         currentHealth = initialHealth;
+
+        updateHealthBar();
+    }
+
+    //updates the healthbar graphics to reflect the current health value
+    void updateHealthBar() {
+
+        if (healthBarManager == null)
+            return;
+
+        //change healthbar value
+        float fractionHealthRemaining = (float)currentHealth / (float)initialHealth;
+        healthBarManager.setHealthValue(fractionHealthRemaining);
     }
 }
